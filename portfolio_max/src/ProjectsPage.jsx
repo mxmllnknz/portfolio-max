575d9b25ff975908b3a4a4e3f477044a9d494225
mxmllnknz <mkunz@cs.hmc.edu>
2024-03-04 13:10:42 -0800

    Scrap costly backend server
    Leave backend server code if performance overhead becomes unbearable
    
    Now just download the small JSON file when loading static page
    
    Truly static site now

--- portfolio_max/src/ProjectsPage.jsx
+++ portfolio_max/src/ProjectsPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import Sidebar from "./components/sidebar";
 import "./ProjectsPage.css";
+import data from "./data/projects.json";
 import {
   Alfie,
   Flycoin,
@@ -13,27 +13,7 @@ import {
 
 function ProjectsPage() {
   const [activeProjectId, setActiveProjectId] = useState(0);
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["projects"],
-    queryFn: async () => {
-      const res = await fetch("/api/projects");
-      if (!res.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      return res.json();
-    },
-    staleTime: Infinity,
-  });
-
   const images = [Flycoin, Inspirit_AI, Jupiter, Memmap, Alfie, Mars_JTS];
-
-  if (isPending) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
 
   return (
     <div className="projects-page-container">
